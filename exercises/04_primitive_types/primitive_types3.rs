4736b77357682b7fe08abf08d4a8accc9b03d33a
ShawnYuan <ysh83737@hotmail.com>
2024-02-06 14:51:52 +0800

    fix: exercises 04 primitive types

--- exercises/04_primitive_types/primitive_types3.rs
+++ exercises/04_primitive_types/primitive_types3.rs
@@ -5,10 +5,10 @@
 // Execute `rustlings hint primitive_types3` or use the `hint` watch subcommand
 // for a hint.
 
-// I AM NOT DONE
+// 语法错误：使用了未定义的数组
 
 fn main() {
-    let a = ???
+    let a = [1; 110];
 
     if a.len() >= 100 {
         println!("Wow, that's a big array!");
